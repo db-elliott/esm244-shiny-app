c0f8bdb8019041ff496a3babbca83cfd483a7344
db-elliott <deanna.elliott19@yahoo.com>
2022-02-03 19:13:40 -0800

    oop

--- group_shiny_app/app.R
+++ group_shiny_app/app.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -62,8 +61,17 @@ ui <- fluidPage(
 )
 
 
+# Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
 }
 
 # Run the application 
