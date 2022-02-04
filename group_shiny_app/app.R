b9694d09af0296fa23abfed850352554cc90c184
db-elliott <deanna.elliott19@yahoo.com>
2022-02-03 19:12:18 -0800

    looking for themes with bslib

--- group_shiny_app/app.R
+++ group_shiny_app/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -60,17 +61,9 @@ ui <- fluidPage(
     )  # end of navbarPage
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    
 }
 
 # Run the application 
