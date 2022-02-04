c30580ea0cf716db6827b500bd0884f70ed440f9
marihdec <marielena@bren.ucsb.edu>
2022-02-03 19:35:37 -0800

    fake output for widget 3

--- group_shiny_app/app.R
+++ group_shiny_app/app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(palmerpenguins)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -38,13 +40,16 @@ ui <- fluidPage(
                         sidebarLayout(
                             sidebarPanel(
                                 radioButtons("radio",
+                                             inputId = "pg_sex",
                                              label = h3("Fish or Coral?"),
-                                             choices = list("Fish", "Coral")),
-                                dateRangeInput("dates",
-                                               label = h3("Select date range"))
+                                             choices = list("male", "female")),
+                                selectInput("select",
+                                            inputId = "pg_year",
+                                            label = h3("Select year"),
+                                            choices = list("2007" = 2007, "2008" = 2008, "2009" = 2009))
                             ), #end of sidebarPanel
                             mainPanel(
-                                
+                                plotOutput(outputId = "pg_plot")
                             ) #end of mainPanel
                         )), #end of sidePanel, W3
                tabPanel("Timescale - Bleaching & Recovery",
@@ -63,8 +68,17 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+    penguin_select <- reactive ({
+        penguins %>%
+            filter(year == input$pg_year) %>%
+            filter(sex == input$pg_sex)
+    })
+    
     # widget 3 output
-    output$value <- renderPrint({ input$dates })
+    output$pg_plot <- renderPlot({
+        ggplot(data = penguin_select(), aes(x = flipper_length_mm, y = body_mass_g)) +
+            geom_jitter(aes(color = species))
+    })
 }
 
 # Run the application 
